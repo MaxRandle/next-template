1748d9e6c08d614332ca82619ea0b4cd5a8b46f3
max <max@kernelwealth.co.nz>
2022-12-07 10:41:08 +1300

    delete console log

--- src/components/Palette/Palette.tsx
+++ src/components/Palette/Palette.tsx
@@ -7,8 +7,6 @@ type PaletteProps = React.ComponentPropsWithoutRef<"div">;
 export const Palette: React.FC<PaletteProps> = ({ className, ...props }: PaletteProps) => {
   const classNames = cn(className, styles.container);
 
-  console.log(styles);
-
   const hues = ["base", "primary", "success", "info", "warning", "danger"] as const;
   const lights = ["50", "100", "200", "300", "400", "500", "600", "700", "800", "900"] as const;
 
